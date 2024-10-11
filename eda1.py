ef502dbeaf0ef0c0a2f3ac267cd897793e2b796e
git_demo <spranith49@gmail.com>
2024-10-11 14:19:47 +0530

    Tip Pred

--- eda1.py
+++ eda1.py
@@ -16,6 +16,8 @@ gender_value = 0 if gender == "Male" else 1
 smoker_value = 0 if smoker == "Yes" else 1
 day_value = {"Thur":0, "Fri":1, "Sat":2, "Sun":3}[day]
 time_value = 0 if time == "Lunch" else 1
+feature7 = 0
+feature8 = 0
 
 if st.button("Predict"):
     result = model.predict([[total_bill, gender_value, smoker_value, day_value, time_value, size, feature7, feature8]])
